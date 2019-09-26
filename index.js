f0bc5de561c8d77161449000e67dd6d1cf10363c
george.burton <george.burton@wework.com>
2019-09-26 23:12:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){  
-  
+  return  Object.assign(object, {[key]: value})
+}
